0189f669288f0c6e9889ecb422a6b996b677ed31
Jakob Nagel <Jakob1211@hotmail.com>
2023-01-25 20:09:16 -0500

    Part 3

---
+++ Stack.py
@@ -0,0 +1,32 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+        self.size = 0
+
+    def push(self, value):
+        self.items.append(value)
+        self.size += 1
+
+    def pop(self):
+        if self.is_empty():
+            print("Empty stack, nothing to pop")
+            return
+        else:
+            self.size -= 1
+            return self.items.pop()
+
+    def top(self):
+        if self.is_empty():
+            print("Empty stack, nothing on top")
+            return
+        else:
+            return self.items[-1]
+
+    def is_empty(self):
+        if len(self.items) == 0:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        return "[" + ",".join(str(item) for item in self.items) + "]"
--- stack_bot.py
+++ stack_bot.py
@@ -1,8 +1,59 @@
+from Stack import Stack
+from typing import List
 
 
 class Bot:
     def __init__(self) -> None:
-        self._data_items = []
+        self._data_items = Stack()
 
     def A(self) -> None:
-        el = self._data_items
+        # Getting the top two integers from stack
+        int1 = self._data_items.pop()
+        int2 = self._data_items.top()
+
+        # Putting the top integer back on to maintain correct positioning
+        self._data_items.push(int1)
+
+        # Sum of top two integers in stack
+        sum = int1 + int2
+
+        # Adding sum of top integers to stack
+        self._data_items.push(sum)
+
+    def T(self) -> None:
+        # The top item in the stack
+        prev = self._data_items.top()
+
+        # Finding the triple the value of the top integer
+        res = prev * 3
+
+        # Adding result to top of stack
+        self._data_items.push(res)
+
+    def D(self) -> None:
+        # Deleting previous integer value from stack
+        self._data_items.pop()
+
+    def I(self, num: int) -> None:
+        # Adds num to top of stack
+        self._data_items.push(num)
+
+    def read_input(self, inpt_str: List[str]) -> List[int]:
+        # Performing operation for each input in input string
+        for inpt in inpt_str:
+            if inpt == 'A':
+                self.A()
+            elif inpt == 'T':
+                self.T()
+            elif inpt == 'D':
+                self.D()
+            else:
+                num = int(inpt)
+                self.I(num)
+
+        return self._data_items
+
+
+string = ["10", "3", "D", "T", "A"]
+bot = Bot()
+print(bot.read_input(string))
